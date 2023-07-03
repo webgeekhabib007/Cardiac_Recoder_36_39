b93172757e8d110360608346750735afce094a02
webgeekhabib007 <73267589+webgeekhabib007@users.noreply.github.com>
2023-07-03 16:31:43 +0600

    update overall ui of the application

--- app/src/main/java/com/example/cardiac_recoder_36_39/LoginActivity.java
+++ app/src/main/java/com/example/cardiac_recoder_36_39/LoginActivity.java
@@ -2,13 +2,80 @@ package com.example.cardiac_recoder_36_39;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etEmail,etPassword;
+    Button btnLogin;
+    TextView tvRegister;
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.editTextEmail1);
+        etPassword = findViewById(R.id.editTextPassword1);
+        tvRegister = findViewById(R.id.textViewRegister);
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString().trim(); // trim() removes spaces
+                String password = etPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    etEmail.setError("Email is empty");
+                }else{
+                    if(!email.contains("@")){
+                        etEmail.setError("Email is invalid");
+                    }else{
+                        if(TextUtils.isEmpty(password)){
+                            etPassword.setError("Password is empty");
+                        }else{
+                            userLogin(email,password);
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void userLogin(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // User login is successful
+                        // You can redirect to the next activity or perform any desired action
+                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this,ListActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // User login failed
+                        // You can display an error message or handle the failure in any desired way
+                        Toast.makeText(LoginActivity.this, "Login failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
